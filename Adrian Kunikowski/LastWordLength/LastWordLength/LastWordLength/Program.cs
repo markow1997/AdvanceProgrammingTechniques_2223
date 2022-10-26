b13440658eb8bf1ab67214ba6c77bf2596e76a3e
Adrian Kunikowski <kunikowskiadrian@onet.pl>
2022-10-26 08:00:14 +0200

    Adding a task solution - pull request  (#35)
    * Update AttendanceList.txt
    
    Removed the commented fragment
    
    * Adding a new folder & my first project
    
    Adding a folder called after my name and surname, and my first project - Roman to Integer (#13 on leetcode)
    
    * Correction of the AttendanceList.txt
    
    Previous change was due to misunderstanding the idea (I think)
    
    * Lista obecnosci - 21.10.2022
    
    * Lista Obecnosci 21.10.2022
    
    Correction - "erasing" myself from the template comment outward.
    
    * Adding a task solution
    
    Adding the solution to "Length of Last Word" from Leetcode

---
+++ Adrian Kunikowski/LastWordLength/LastWordLength/LastWordLength/Program.cs
@@ -0,0 +1,20 @@
+﻿ 
+internal class Program
+{
+    private static int Main(string[] args)
+    {
+        int LengthOfLastWord(string s)
+        {
+            string noTrails = s.Trim();
+            string[] words = noTrails.Split(" ");
+            return words[(words.Length - 1)].Length;
+        }
+
+        string exampleStr = "  This sentence is filled with a lot of   spaces and other distractions   ";
+
+        Console.WriteLine("The length of the last word of string \""+exampleStr+"\" is "+LengthOfLastWord(exampleStr) );
+        Console.ReadKey();
+
+        return 0;
+    }
+}
