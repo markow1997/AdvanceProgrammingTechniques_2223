5e6d0229e73f2e4fc9722a04765fbdfdbd8f2d71
Adrian Kunikowski <kunikowskiadrian@onet.pl>
2022-11-13 14:27:09 +0100

    Adding PalindromeNumber solution (#124)
    Problem number 9 on Leetcode, as part of our homework from previous classes (04.11)

---
+++ Adrian Kunikowski/PalindromeNumber/PalindromeNumber/PalindromeNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    internal class PalindromeNumber
+    {
+
+        static void Main(string[] args)
+        {
+            int[] testowyInt = new int[7] { 121, -121, 0, 1555352, -252525, 10, -1};
+
+            foreach(int number in testowyInt)
+            {
+                if (IsPalindrome(number))
+                    Console.WriteLine(number + " jest palindromem");
+                else
+                    Console.WriteLine(number + " nie jest palindromem");
+            }
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            string str = x.ToString();
+
+            int i = 0; int j = str.Length;
+            while (i < j - 1)
+            {
+                if (str[i] != str[j - 1])
+                    return false;
+                else
+                {
+                    i++;
+                    j--;
+                }
+            }
+            return true;
+        }
+    }
+}
