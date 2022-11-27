39df7291b7b3593afb4b79819b079f97871c6ac9
Monikazx <UL0234805@edu.uni.lodz.pl>
2022-11-27 02:21:02 +0100

    242. Valid Anagram (#212)

---
+++ MonikaMostek/242_Valid_Anagram.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anagram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write first anagram to check: ");
+            string word1 = Console.ReadLine();
+            Console.WriteLine("Write second anagram to check: ");
+            string word2 = Console.ReadLine();
+            List<char> l1 = new List<char>();
+            List<char> l2 = new List<char>();
+            if(word1.Length != word2.Length)
+            {
+                Console.WriteLine("It is not an anagram");
+            }
+            else
+            {
+                l1 = word1.ToList();
+                l2 = word2.ToList();
+                l1.Sort();
+                l2.Sort();
+                bool anagram = true;
+                for(int i = 0; i < word1.Length; i++)
+                {
+                    if(l1[i] != l2[i])
+                    {
+                        Console.WriteLine("It is not an anagram");
+                        anagram = false;
+                        break;
+                    }
+                }
+                if (anagram == true)
+                {
+                    Console.WriteLine("It is an anagram");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
