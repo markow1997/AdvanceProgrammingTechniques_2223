d29b07e0b74a5aefd8aa4e6f9b05f6cf46fd45dc
Adrian Kunikowski <UL0243412@edu.uni.lodz.pl>
2023-01-13 17:25:23 +0100

    35. Search Insert Position (#402)
    Adding solution to Search Insert Position problem number 35

---
+++ Adrian Kunikowski/SearchInsertPosition/SearchInsertPosition/SearchInsertPosition.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace SearchInsertPosition
+{
+    class Program
+    {
+
+        static public int SearchInsert(int[] nums, int target)
+        {
+            int i = 0;
+            for (i = 0; i < nums.Count(); i++)
+            {
+                if (nums[i] >= target)
+                {
+                    return i;
+                }
+            }
+            return i;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] tab = new int[4] { 1, 3, 5, 6 };
+            int szukaj = 4;
+            Console.WriteLine("Gdyby liczba "+szukaj+" byla w tablicy, to bylaby na pozycji "+SearchInsert(tab,szukaj));
+        }
+    }
+}
